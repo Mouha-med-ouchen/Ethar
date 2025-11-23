caba99bd0bdf090ac6461a95a989bb702c7365aa
Mohamed Ouchen <ouchenmohammed963@gmail.com>
2025-11-23 11:02:33 +0100

    run dev

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-// الحصول على العنصر root من الـ HTML
 const rootElement = document.getElementById('root')
 
-// إنشاء root جديد وتشغيل التطبيق
 createRoot(rootElement).render(
   <StrictMode>
     <App />
